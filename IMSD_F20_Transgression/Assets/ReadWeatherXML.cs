c2bc9174d5904b6e945d91f4ac2b64965151297c
Arash Izadi <arash.iz@live.com>
2020-10-05 13:24:32 -0400

    Added weather map API

---
+++ IMSD_F20_Transgression/Assets/ReadWeatherXML.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Xml;
+using UnityEngine.Networking;
+public class ReadWeatherXML : MonoBehaviour
+{
+    string key = "429bad3aa020d768da6a26b47e01445f";
+    string apiReturn = "";
+    void Start()
+    {
+        StartCoroutine(GetWeather());
+    }
+    IEnumerator GetWeather()
+    {
+        UnityWebRequest www = UnityWebRequest.
+            Get("http://api.openweathermap.org/data/2.5/weather?zip=10303&mode=xml&APPID=" + key);
+        yield return www.SendWebRequest();
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            // Get text content like this:
+            Debug.Log(www.downloadHandler.text);
+            apiReturn = www.downloadHandler.text;
+        }
+        else
+        {
+            Debug.Log(www.error + " " + www);
+        }
+    }
+}
