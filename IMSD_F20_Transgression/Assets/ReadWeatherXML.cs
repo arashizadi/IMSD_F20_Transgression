1903ef10a16645ec9dcd14e8c1795ea5a7860d18
Arash Izadi <arash.iz@live.com>
2020-10-09 16:15:06 -0400

    Added Rain Detection and other codes

--- IMSD_F20_Transgression/Assets/ReadWeatherXML.cs
+++ IMSD_F20_Transgression/Assets/ReadWeatherXML.cs
@@ -4,14 +4,69 @@ using UnityEngine;
 using System;
 using System.Xml;
 using UnityEngine.Networking;
+using UnityEngine.UI;
+
 public class ReadWeatherXML : MonoBehaviour
 {
     string key = "429bad3aa020d768da6a26b47e01445f";
     string apiReturn = "";
     public string zip = "11355";
+    public GameObject rainEmitter;
+    public Text text;
+    private string _zip;
     void Start()
     {
+        _zip = zip;
         StartCoroutine(GetWeather());
+    }
+
+    void Update()
+    {
+        if (zip.Length == 5 && _zip != zip)
+        {
+            StartCoroutine(GetWeather());
+            _zip = zip;
+        }
+        if (apiReturn.Contains("mode=\"rain\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Rainy";
+        }
+        else if (apiReturn.Contains("mode=\"snow\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Snowy";
+        }
+        else if (apiReturn.Contains("mode=\"clear\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Clear Sky";
+        }
+        else if (apiReturn.Contains("mode=\"thunderstorm\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Thunderstorm";
+        }
+        else if (apiReturn.Contains("mode=\"drizzle\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Drizzle";
+        }
+        else if (apiReturn.Contains("mode=\"atmosphere\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Atmosphere";
+        }
+        else if (apiReturn.Contains("mode=\"clouds\""))
+        {
+            rainEmitter.SetActive(true);
+            text.text = "Cloudy";
+        }
+        else
+        {
+            rainEmitter.SetActive(false);
+            text.text = "Nice Weather Outside!";
+        }
     }
     IEnumerator GetWeather()
     {
